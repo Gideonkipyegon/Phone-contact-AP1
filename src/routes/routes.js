71987c1783f484aab01e7e6bc1a7739f4bee2a53
sAndrin3 <sandrinemalanga2001@gmail.com>
2023-06-16 09:10:27 +0300

    auth

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,12 +7,19 @@ import {
   assignGroup,
   deleteUser,
 } from "../controllers/phoneContoller.js";
+import { login, register, loginRequired} from '../controllers/userController.js';
 
 const user = (app) => {
-  app.route("/users").get(allUsers).post(createUser);
+  app.route("/users").get(loginRequired, allUsers).post(loginRequired, createUser);
   app.route("/groups").get(allGroups);
 
-  app.route("/users/:id").get(getSingleUser).put(updateUser).delete(deleteUser);
+  app.route("/users/:id").get(loginRequired, getSingleUser).put(loginRequired, updateUser).delete(loginRequired, deleteUser);
   app.route("/groups/:id").put(assignGroup);
+
+  app.route('/auth/register')
+     .post(register);
+
+  app.route('/auth/login')
+     .post(login)
 };
 export default user;
